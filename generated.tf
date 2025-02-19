2d94f19ba3acb3c8e567a14a41a55b82557e2dfc
matthew graham <matthew.graham@abrigo.com>
2025-02-19 18:42:54 -0500

    Initial commit

---
+++ generated.tf
@@ -0,0 +1,25 @@
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by Terraform from "sg-06026ac09e6fe6a05"
+resource "aws_security_group" "ingress" {
+  description = "Security group with no ingress rule"
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+  ingress                = []
+  name                   = "no-ingress-sg"
+  name_prefix            = null
+  revoke_rules_on_delete = null
+  tags                   = {}
+  tags_all               = {}
+  vpc_id                 = "vpc-0a319c022ba41f660"
+}
---
+++ imports.tf
@@ -0,0 +1,48 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-0a319c022ba41f660" #VPC
+# }
+#
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-0fa74c0306f1f0955" #PublicSubnet1
+# }
+#
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-09922dd6383b1c553" #PublicSubnet2
+# }
+#
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-0141f5547405063d3" #InternetGateway
+# }
+#
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-09baedefc41a6e9ef_0.0.0.0/0" #DefaultPublicRoute
+# }
+#
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-09baedefc41a6e9ef" #PublicRouteTable
+# }
+#
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-0fa74c0306f1f0955/rtb-09baedefc41a6e9ef" #PublicSubnet1/PublicRouteTable
+# }
+#
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-09922dd6383b1c553/rtb-09baedefc41a6e9ef" #PublicSubnet2/PublicRouteTable
+# }
+#
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-06026ac09e6fe6a05" #NoIngressSecurityGroup
+# }
